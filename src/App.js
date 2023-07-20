d6657fb9ef522bfd5236d194c8e10d863925872c
kameoka <e2058232@soka-u.jp>
2023-07-20 10:53:42 +0900

    station04

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+// import React, { useState } from 'react'
+
 import './App.css'
 
 /**
  *
  * @type {React.FC}
  */
+
+this.state = {
+  dogUrl: 'https://images.dog.ceo/breeds/deerhound-scottish/n02092002_2558.jpg',
+}
+
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(0)
   return (
     <div>
       <header>
