4386bb6f68693a36af69c62a4a7555203f7e6e3b
kameoka <e2058232@soka-u.jp>
2023-07-21 12:55:55 +0900

    station05

--- src/App.js
+++ src/App.js
@@ -18,9 +18,23 @@ export const App = () => {
         <h1>Dogアプリ</h1>
       </header>
       <main>
-        <p>犬の画像を表示するサイトです</p>
-        {/* <img src="https://images.dog.ceo/breeds/deerhound-scottish/n02092002_2558.jpg" /> */}
-        <img src={dogUrl} />
+        <div>
+          <p>犬の画像を表示するサイトです</p>
+        </div>
+        <div>
+          <img src={dogUrl} />
+        </div>
+        <div>
+          <button
+            onClick={() =>
+              setDogUrl(
+                'https://images.dog.ceo/breeds/weimaraner/n02092339_514.jpg',
+              )
+            }
+          >
+            更新
+          </button>{' '}
+        </div>
       </main>
     </div>
   )
