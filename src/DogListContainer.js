c4bee652c9f649455241cb5d013fc9a497acd59d
kameoka <e2058232@soka-u.jp>
2023-07-25 18:45:47 +0900

    statoin09

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,11 +1,14 @@
 import * as React from 'react'
 
 export function DogListContainer() {
-  const [breeds, setBreeds] = React.useState(0)
+  const [breeds, setBreeds] = React.useState(null)
   React.useEffect(() => {
-    let getbreeds = fetch('https://dog.ceo/api/breeds/list/all')
-    getbreeds = getbreeds.json()
-    setBreeds(getbreeds.message)
+    ;() => {
+      let getbreeds = fetch('https://dog.ceo/api/breeds/list/all')
+      getbreeds = getbreeds.json()
+      getbreeds = Object.keys(getbreeds.message)
+      setBreeds(getbreeds)
+    }
   }, [])
   return <p>{breeds}</p>
 }
